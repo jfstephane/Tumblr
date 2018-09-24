9d88d27bdf017bec70d7153fc3f800e17e0223c7
Jules Frantz Stephane Loubeau <stelou29@gmail.com>
2018-09-24 11:33:59 -0400

    fullscreenView
    Full Screen

--- Tumblr/FullScreenPhotoViewController.swift
+++ Tumblr/FullScreenPhotoViewController.swift
@@ -1,37 +1,31 @@
 //
-//  PhotoDetailsViewController.swift
+//  FullScreenPhotoViewController.swift
 //  Tumblr
 //
-//  Created by Jules Frantz Stephane Loubeau on 9/22/18.
+//  Created by Jules Frantz Stephane Loubeau on 9/23/18.
 //  Copyright © 2018 Jules Frantz Stephane Loubeau. All rights reserved.
 //
 
 import UIKit
 import AlamofireImage
 
-class PhotoDetailsViewController: UIViewController {
-
-    @IBOutlet weak var photoImageView: UIImageView!
+class FullScreenPhotoViewController: UIViewController {
     
+    @IBOutlet weak var fullScreen: UIImageView!
     var imageURL: URL!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        fullScreen.af_setImage(withURL: imageURL!)
 
         // Do any additional setup after loading the view.
-        photoImageView.af_setImage(withURL: imageURL!)
-        
-        photoImageView.isUserInteractionEnabled = true
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        let destinationVC = segue.destination as! FullScreenPhotoViewController
-        destinationVC.imageURL = imageURL
     }
     
 
